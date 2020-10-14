30f7dc84afd71a29bbd682adb57133c9b14f73c4
east196 <2901180515@qq.com>
2020-10-14 20:34:00 +0800

    + gateway

---
+++ playground-spring-cloud/gateway/src/test/java/com/github/east196/playground/springcloud/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.east196.playground.springcloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
