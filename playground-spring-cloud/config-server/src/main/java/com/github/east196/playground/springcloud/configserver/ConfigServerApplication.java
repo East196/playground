26b0faec4ff2b735a5a19441f3e18a1b1b95b899
east196 <2901180515@qq.com>
2020-10-16 16:28:39 +0800

    + config-server

---
+++ playground-spring-cloud/config-server/src/main/java/com/github/east196/playground/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.github.east196.playground.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ playground-spring-cloud/config-server/src/test/java/com/github/east196/playground/springcloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.east196.playground.springcloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
