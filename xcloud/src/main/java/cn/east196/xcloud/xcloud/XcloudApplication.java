60c21c913f74690a868953f04e1074c28d13b09d
East196 <2901180515@qq.com>
2021-05-17 00:08:27 +0800

    ^ spring cloud 2020.0.2

---
+++ xcloud/src/main/java/cn/east196/xcloud/xcloud/XcloudApplication.java
@@ -0,0 +1,13 @@
+package cn.east196.xcloud.xcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcloudApplication.class, args);
+	}
+
+}
