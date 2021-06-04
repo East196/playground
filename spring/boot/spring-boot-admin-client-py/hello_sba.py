e02b52b7945bcf9042af76f36b400ccd6c081541
East196 <2901180515@qq.com>
2021-06-04 21:03:05 +0800

    + docker-compose

--- spring/boot/spring-boot-admin-client-py/hello_sba.py
+++ spring/boot/spring-boot-admin-client-py/hello_sba.py
@@ -3,6 +3,7 @@
 
 import os
 from flask import Flask
+from pyctuator.auth import BasicAuth
 from pyctuator.pyctuator import Pyctuator
 
 app_name = "Flask App with Pyctuator"
@@ -17,9 +18,10 @@ def hello():
 Pyctuator(
     app,
     app_name,
-    app_url="http://localhost:5000",
-    pyctuator_endpoint_url="http://localhost:5000/pyctuator",
-    registration_url="http://localhost:9000/instances"
+    app_url="http://192.168.99.100:5000",
+    pyctuator_endpoint_url="http://192.168.99.100:5000/pyctuator",
+    registration_url="http://192.168.99.100:9600/instances",
+    registration_auth=BasicAuth(username="admin",password="admin"),
 )
 
-app.run()
+app.run(host="0.0.0.0")
